3f1408e71724126dfb1e14c3a997127cdcf18622
Richard Meats <35453720+rjmeats@users.noreply.github.com>
2018-08-16 15:53:39 +0100

    Create playlist

--- SpotifyRestBash/callcurl.sh
+++ SpotifyRestBash/callcurl.sh
@@ -42,12 +42,18 @@ function outputFilePreparation() {
 
 # Invoke curl for a specific verb/entrypoint combination. Record response in the specified output file.
 
+# Simpler form for when there is no data to pass in the request, other than in URL parameters
 function callCurl() {
+	callCurlFull "$1" "$2" "-" "$3" "$4"
+}
+
+function callCurlFull() {
 
 	local VERB="$1"
 	local ENDPOINT="$2"
-	local OUTFILE_PARAM="$3"			# Expected to end .json and include path info
-	local QUIETMODE="${4:-N}"			# Suppress some output e.g. if called within a paging operation
+	local REQUEST_DATA="$3"				# Expected to be in json format. If it starts @ then it is a file name
+	local OUTFILE_PARAM="$4"			# Expected to end .json and include path info
+	local QUIETMODE="${5:-N}"			# Suppress some output e.g. if called within a paging operation
 
 	local URL_BASE="https://api.spotify.com"
 	local TOKEN=$(cat token.txt)			# Needs to be updated when the previous token expires or has insufficient access
@@ -66,6 +72,11 @@ function callCurl() {
 
 	SUMMARY="${VERB} request to ${FULL_URL}"
 
+	if [[ "$REQUEST_DATA" != "-" ]]
+	then
+		printf -v SUMMARY "%s\r\nRequest Data:\r\n%s" "${SUMMARY}" "$(echo ${REQUEST_DATA} | jq '.')"	# Pipe request into jq to get prettier output
+	fi
+
 	if [[ ${QUIETMODE} == "Y" ]]
 	then
 		echo "${SUMMARY}" > "${REQUEST_OUTFILE}"
@@ -77,7 +88,15 @@ function callCurl() {
 	fi
 	{
 		# -i displays returned headers. -s is silent mode. -D puts headers into a separate file
-		curl -s -X "${VERB}" "${FULL_URL}" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer ${TOKEN}" -D "${HEADER_OUTFILE}" 
+		REQ=""
+		if [[ "$REQUEST_DATA" == "-" ]]
+		then
+			curl -s -X "${VERB}" "${FULL_URL}" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer ${TOKEN}" -D "${HEADER_OUTFILE}"
+		else
+			REQ="-d ${REQUEST_DATA}"
+			curl -s -X "${VERB}" "${FULL_URL}" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer ${TOKEN}" -D "${HEADER_OUTFILE}" "${REQ}" 
+		fi
+
 		CURLSTATUS=$?
 	} > ${OUTFILE}
 
@@ -279,7 +298,6 @@ function callCurlPaging() {
 					# echo "${NEXT_URL} is the next URL"
 					NEXT_URL=$(removeSurroundingDoubleQuotes ${NEXT_URL})
 					# echo "${NEXT_URL} is the next URL"
-					ENDPOINT="${NEXT_URL}"
 
 					if (( ITEMS_SO_FAR < MAXITEMS ))
 					then
@@ -302,12 +320,13 @@ function callCurlPaging() {
 					# Fetch some more
 					let CALL_COUNT+=1
 					local MY_OUTFILE=${FINAL_OUTFILE}.${CALL_COUNT}
-					callCurl "${VERB}" "${ENDPOINT}" "${MY_OUTFILE}" Y
+					callCurl "${VERB}" "${NEXT_URL}" "${MY_OUTFILE}" Y
 					
 					if [[ $? -eq 0 ]]
 					then
 						cat ${OUTFILE} | unix2dos >> $FINAL_OUTFILE
 						cat ${HEADER_OUTFILE} | unix2dos >> $FINAL_HEADER_OUTFILE
+						echo "Next page : " >> $FINAL_REQUEST_OUTFILE
 						cat ${REQUEST_OUTFILE} | unix2dos >> $FINAL_REQUEST_OUTFILE
 					else
 						echo
--- SpotifyRestBash/formatutils.sh
+++ SpotifyRestBash/formatutils.sh
@@ -7,17 +7,17 @@
 
 function showItemNames() {
 	local OUTPUT_FILE="$1"	# Where the json output is held
-	local ITEM_TYPE="$2"	# The name of the element which contains the items[] array, e.g. albums
+	local ITEM_TYPE="$2"	# The name of the element which contains the items[] array, e.g. albums, or can be just '.'
 	local MAX_ITEMS_TO_DISPLAY="$3"
 
-	if [[ $ITEM_TYPE == "categories" ]]
+	local FOUND_ITEMS
+	if [[ $ITEM_TYPE != "." ]]
 	then
-		ITEM_TYPE_SINGULAR="category"
+		FOUND_ITEMS=$(cat ${OUTPUT_FILE} | jq ".${ITEM_TYPE}.items[]?.name")
 	else
-		ITEM_TYPE_SINGULAR=${ITEM_TYPE%s}
-	fi	
-
-	local FOUND_ITEMS=$(cat ${OUTPUT_FILE} | jq ".${ITEM_TYPE}.items[]?.name")
+		# Top level item
+		FOUND_ITEMS=$(cat ${OUTPUT_FILE} | jq ".items[]?.name")
+	fi
 
 	declare -i FOUND_ITEM_COUNT
 
@@ -29,6 +29,20 @@ function showItemNames() {
 		FOUND_ITEM_COUNT=$(echo "$FOUND_ITEMS" | wc -l)
 	fi
 	
+	# Change the top-level item type indicator from '.'  to 'items' for producing output text
+	if [[ $ITEM_TYPE == "." ]]
+	then
+		ITEM_TYPE="items"
+	fi
+
+	# Spotify uses plural attribute names for its objects. Produce a singular form too, by removing the final
+	# 's' unless it's a known special case.
+	ITEM_TYPE_SINGULAR=${ITEM_TYPE%s}
+	if [[ $ITEM_TYPE_SINGULAR == "categorie" ]]
+	then
+		ITEM_TYPE_SINGULAR="category"
+	fi	
+
 	echo
 	if (( FOUND_ITEM_COUNT == 1 ))
 	then
